11ec6072ddfffa1432018ab49136dfa34ca77efe
BREND3112317 <brend.main@gmail.com>
2021-10-11 13:44:28 +0800

    Update: Install react-copy-to-clipboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import { Route } from 'react-router-dom';
 import { Dashboard } from './component/dashboard';
 import { InputForm } from './component/InputForm';
 
+const routes = [
+  {
+    path: '/:id',
+    component: InputForm,
+    exact: true,
+    breadcrumbName: "Input",
+    // routes: [
+    //   {
+    //     path: '/:id/dashboard',
+    //     component: Dashboard,
+    //     breadcrumbName: "Dashboard"
+    //   }
+    // ]
+  },
+  {
+    path: '/:id/dashboard',
+    component: Dashboard,
+    breadcrumbName: "Dashboard"
+  }
+]
+
 class App extends Component {
   componentDidMount() {
     this.callBackendAPI().then(res => this.setState({data: res.express})).catch(err => console.log(err));
@@ -21,8 +42,23 @@ class App extends Component {
   render() {
     return (
         <div className="container">
-            <Route path="/:id" exact component={InputForm}/>
-            <Route path="/:id/dashboard" component={Dashboard}/>
+            {
+              routes.map((route, i) => {
+                const { path, exact, routes } = route;
+                return(
+                  <Route
+                    key={i}
+                    path={path}
+                    exact={exact}
+                    render={(routeProps) => (
+                      <route.component routes={routes} {...routeProps}/>
+                    )}
+                  />
+                )
+              })
+            }
+            {/* <Route path="/:id" exact component={InputForm}/>
+            <Route path="/:id/dashboard" component={Dashboard}/> */}
         </div>
     );
   }
